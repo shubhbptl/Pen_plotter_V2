f359a5d02a4c6f39e9b6c01a82ff36655c30b9b7
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2023-09-30 19:46:27 -0400

    Added a simple markdown renderer to the page.

--- my_flask/static/js/main.js
+++ my_flask/static/js/main.js
@@ -1,3 +1,10 @@
-window.onload = function(){
-	alert('.js file was loaded!');
-}
+const marked = window.marked;
+console.log(marked);
+const request = new Request(
+	"/static/markdown.md",
+	{}
+);
+const response = await fetch(request);
+const markdown = await response.text();
+const html = marked.parse(markdown);
+document.querySelector('#markdown').innerHTML = html;
