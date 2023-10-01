04ac89f711f9555375831be02202596af57b7bc7
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2023-10-01 12:24:02 -0400

    Improved website formatting around gcode preview.

--- my_flask/static/js/main.js
+++ my_flask/static/js/main.js
@@ -10,12 +10,13 @@ const html = marked.parse(markdown);
 document.querySelector('#markdown').innerHTML = html;
 
 const preview = (window.preview = new GCodePreview.init({
-    canvas: document.querySelector('canvas'),
-    // lineWidth: 4,
-    buildVolume: {x: 150, y: 150, z: 150},
-    initialCameraPosition: [0,400,450],
-    // debug: true
-    allowDragNDrop: true
-  }));  // draw a diagonal line
-  const gcode = 'G0 X0 Y0 Z0.2\nG1 X42 Y42 E10';
-  preview.processGCode(gcode);
+	canvas: document.querySelector('canvas'),
+	lineWidth: 4,
+	buildVolume: {x: 150, y: 150, z: 150},
+	initialCameraPosition: [0,400,450],
+	//debug: true,
+	allowDragNDrop: true,
+	renderTravel: true
+}));  // draw a diagonal line
+const gcode = ''; 
+preview.processGCode(gcode);
