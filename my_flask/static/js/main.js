e13dab4a905729d37eb3121f9f72af2ec60517b2
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2023-10-01 19:43:02 -0400

    Added functionality to input img files and automatically scale them.

--- my_flask/static/js/main.js
+++ my_flask/static/js/main.js
@@ -26,14 +26,51 @@ preview.processGCode(gcode);
 
 
 
-document.getElementById('inputfile')
-            .addEventListener('change', function() {
-              
-            var fr=new FileReader();
-            fr.onload=function(){
-		preview.processGCode(fr.result);	    
-	    }
-              
-            fr.readAsText(this.files[0]);
-		
+/*	The following code handle the selection of files,
+*	if the file is an image it first converts it to 
+*	gcode and is thus later able to directly send the
+*	gcode file to the server.
+*/
+
+let gcodeFile;
+let svgFile;
+let imageFile;
+
+document.getElementById('inputfile').addEventListener('change', function() {
+	let file = this.files[0];
+	let fr = new FileReader();
+	if(file.type === "text/x.gcode"){
+		fr.onload = function(){
+			let result = fr.result;
+			preview.processGCode(result);
+		}
+		fr.readAsText(file);
+	}else if(
+		file.type === "image/jpeg" ||
+		file.type === "image/bmp" ||
+		file.type === "image/png"
+	){
+		fr.onload = function(){
+			let result = fr.result;
+			console.log(result);
+			console.log("Image selected");
+			console.log(file);
+
+			const img = document.querySelector('img');	
+			img.src = result;
+
+			img.onload = function(){
+				const canvas = document.createElement("canvas");
+				const ctx = canvas.getContext("2d");
+
+				canvas.width = 200;
+				canvas.height = 150;
+
+				ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+				const dataURI = canvas.toDataURL();
+				img.src = dataURI;
+			}
+		}
+		fr.readAsDataURL(file);
+	}
 });
