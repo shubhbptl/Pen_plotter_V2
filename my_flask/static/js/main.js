bba3ec0b43de93fb8c5f36f74795a2b8e23e3c6b
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2023-10-01 11:51:39 -0400

    Added a gcode preview window to main page.

--- my_flask/static/js/main.js
+++ my_flask/static/js/main.js
@@ -8,3 +8,14 @@ const response = await fetch(request);
 const markdown = await response.text();
 const html = marked.parse(markdown);
 document.querySelector('#markdown').innerHTML = html;
+
+const preview = (window.preview = new GCodePreview.init({
+    canvas: document.querySelector('canvas'),
+    // lineWidth: 4,
+    buildVolume: {x: 150, y: 150, z: 150},
+    initialCameraPosition: [0,400,450],
+    // debug: true
+    allowDragNDrop: true
+  }));  // draw a diagonal line
+  const gcode = 'G0 X0 Y0 Z0.2\nG1 X42 Y42 E10';
+  preview.processGCode(gcode);
